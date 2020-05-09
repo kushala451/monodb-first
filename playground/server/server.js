dd54a5d34d22f46978076755ec028da9c1d9c5b5
kushala <kushala451@gmai.com>
2020-05-09 13:05:15 +0530

    'serverchange'

--- playground/server/server.js
+++ playground/server/server.js
@@ -1,5 +1,6 @@
 var express =require('express');
 var bodyparser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Tobe} = require('./models/tobe');
@@ -29,6 +30,28 @@ app.get('/tobes', (req,res) =>{
         res.status(400).send(e);
     })
 });
+
+app.get('/tobes/:id',(req,res) =>{
+    // res.send(req.params);
+    var id =req.params.id;
+
+    if(!ObjectID.isValid(id)){
+return res.status(400).send();
+    }
+    Tobe.findById(id).then((Tobe) => {
+ if(!Tobe){
+
+    res.status((400).send);
+ }
+  res.send({Tobe});
+         
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+
+
 
 app.listen(3000, () =>{
     console.log('started on port 3000');
